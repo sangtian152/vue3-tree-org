f718f98ac04712de818a648dc67035c395d3c435
sangtian152 <jzyazn@sina.cn>
2022-02-23 20:26:10 +0800

    修复一些问题

--- src/components/node/index.ts
+++ src/components/node/index.ts
@@ -117,8 +117,12 @@ export const renderLabel = (h:any, data:INode, context:any, root:boolean) => {
   const nodeLabelClass = ['tree-org-node__content']
   if (root) {
     nodeLabelClass.push('is-root')
-  } else if (data.newNode) {
-    nodeLabelClass.push('is-new')
+  }
+  if (!data[defaultProps.label]) {
+    nodeLabelClass.push('is-empty')
+  }
+  if (data.focused) {
+    nodeLabelClass.push('is-edit')
   }
   // directives
   // let cloneDirs
