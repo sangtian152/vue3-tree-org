c32e769d5b8153d1c8569f13a8898c3b0b6d45d0
sangtian152 <jzyazn@sina.cn>
2023-01-13 16:47:57 +0800

    update

--- docs/.vuepress/.temp/pages/guide/index.html.js
+++ docs/.vuepress/.temp/pages/guide/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-fffb8e28\",\"path\":\"/guide/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"介绍\",\"slug\":\"介绍\",\"children\":[]},{\"level\":3,\"title\":\"安装\",\"slug\":\"安装\",\"children\":[]},{\"level\":3,\"title\":\"引入\",\"slug\":\"引入\",\"children\":[]},{\"level\":3,\"title\":\"源码\",\"slug\":\"源码\",\"children\":[]},{\"level\":3,\"title\":\"最新版本\",\"slug\":\"最新版本\",\"children\":[]}],\"git\":{\"updatedTime\":1644368909000,\"contributors\":[{\"name\":\"sangtian152\",\"email\":\"jzyazn@sina.cn\",\"commits\":7}]},\"filePathRelative\":\"guide/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
